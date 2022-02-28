c8291d606c2e65805029ce822f987819c96a6cce
Gohuteur <88322073+Gohuteur@users.noreply.github.com>
2022-02-28 17:45:26 +0100

    Truc

--- Assets/Script/Player/Raycast.cs
+++ Assets/Script/Player/Raycast.cs
@@ -6,8 +6,7 @@ public class Raycast : MonoBehaviour
 {
     
     [SerializeField] float distancehit = 1.0f;
-   
-
+    [SerializeField] Animator anim;
     public bool hasKey=false;
     void Update()
     {
@@ -16,7 +15,7 @@ public class Raycast : MonoBehaviour
       
         if (Physics.Raycast(transform.position, transform.forward, out hit, distancehit) && hit.collider.gameObject.tag =="Copie" || hit.collider.gameObject.tag =="Clé" || hit.collider.gameObject.tag =="Porte" )
         {
-            Debug.Log("LOL");
+            
            
             if (hit.collider.gameObject.tag =="Clé" && Input.GetKey("e")) 
             {
@@ -25,7 +24,11 @@ public class Raycast : MonoBehaviour
             }
             if (Input.GetKey("e") && hit.collider.gameObject.tag =="Porte" && hasKey==true )
             {
-                hit.collider.gameObject.transform.Translate(3,0,0);
+                Debug.Log("lol");
+                anim.SetBool("bouge",true);
+                hasKey=false;
+
+                
             }
         }
     }
