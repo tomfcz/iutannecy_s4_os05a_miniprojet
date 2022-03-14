4fd1bb6d990b4e443291a38d3c8195ee6023de0c
Gohuteur <88322073+Gohuteur@users.noreply.github.com>
2022-03-14 23:09:00 +0100

    Perso dans la scène finale

--- Assets/Script/Player/Raycast.cs
+++ Assets/Script/Player/Raycast.cs
@@ -7,10 +7,7 @@ public class Raycast : MonoBehaviour
 {
     
     [SerializeField] float distancehit = 1.0f;
-    [SerializeField] Animator animporteD360;
-    [SerializeField] Animator animporteD361;
-    [SerializeField] Animator animporteD350;
-    [SerializeField] Animator animporteD352;
+    public Animator[] animporte;
     [SerializeField] Animator animporteSalleDesPROF;
     [SerializeField] Animator secporte1;
     [SerializeField] Animator secporte2;
@@ -37,25 +34,25 @@ public class Raycast : MonoBehaviour
             if (Input.GetKey("e") && hit.transform.gameObject.tag =="PorteD360" && hasKey==true )
             {
                 Debug.Log("lol");
-                animporteD360.SetBool("bouge",true);
+                animporte[0].SetBool("bouge",true);
                 hasKey =false;
             }
             if (Input.GetKey("e") && hit.transform.gameObject.tag == "Porte361" && hasKey == true)
             {
                 Debug.Log("lol");
-                animporteD361.SetBool("bouge", true);
+                animporte[1].SetBool("bouge", true);
                 hasKey = false;
             }
             if (Input.GetKey("e") && hit.transform.gameObject.tag == "PorteD350" && hasKey == true)
             {
                 Debug.Log("lol");
-                animporteD350.SetBool("bouge", true);
+                animporte[2].SetBool("bouge", true);
                 hasKey = false;
             }
             if (Input.GetKey("e") && hit.transform.gameObject.tag == "PorteD352" && hasKey == true)
             {
                 Debug.Log("lol");
-                animporteD352.SetBool("bouge", true);
+                animporte[3].SetBool("bouge", true);
                 hasKey = false;
             }
 
