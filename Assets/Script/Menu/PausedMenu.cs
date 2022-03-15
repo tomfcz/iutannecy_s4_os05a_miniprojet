6367f0210807a9c08b96288659874c98b0db70a7
RobinD74 <63494849+RobinD74@users.noreply.github.com>
2022-03-15 15:28:41 +0100

    Rangement

--- Assets/Script/Menu/PausedMenu.cs
+++ Assets/Script/Menu/PausedMenu.cs
@@ -6,7 +6,61 @@ using UnityEngine.SceneManagement;
 
 public class PausedMenu : MonoBehaviour
 {
-    [SerializeField] GameObject pausedMenu;
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+
+    void Updatee()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            } else {
+                Paused();
+            }
+        }
+    }
+
+    public void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gamePaused = true;
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        gamePaused = false;
+    }
+
+    public void GoToMainMenu()
+    {
+        if (EditorUtility.DisplayDialog("Return to the main menu ?",
+            "Are you sure you want to return to the main menu ?\n" +
+            "Your progression will not be saved.",
+            "Yes", "No"))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("Main menu");
+            gamePaused = false;
+        }
+
+    }
+
+    public void Quit()
+    {
+        if (EditorUtility.DisplayDialog("Quit the game ?",
+            "Are you sure you want to quit the game ?\n" +
+            "Your progression will not be saved.",
+            "Yes", "No"))
+        {
+            Application.Quit();
+        }
+    }
+
+    /*[SerializeField] GameObject pausedMenu;
     public static bool isPaused;
     public GameObject pauseButton;
 
@@ -60,8 +114,8 @@ public class PausedMenu : MonoBehaviour
     public void test()
     {
         Debug.Log("test oui");
-    }
-    
+    }*/
+
 }
 
 
